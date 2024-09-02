6ffdefba3abb20fdccdf13975af0cdc63b6c4c05
Vanisper <273266469@qq.com>
2024-09-02 22:52:56 +0800

    feat(all): initial commit

---
+++ src/pages-sub/demo/index.vue
@@ -0,0 +1,17 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: '物品摆放demo' },
+}
+</route>
+
+<template>
+  <view class="text-center"></view>
+</template>
+
+<script lang="ts" setup>
+// code here
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
